cb465c0c3aa3bd30889268f10192937b519d4218
Ambursley <25256015+NoteNerdOfficial@users.noreply.github.com>
2026-08-24 00:58:46 -0400

    Use the brand icon for the menu bar item instead of a generic SF Symbol

--- Clip/ClipApp.swift
+++ Clip/ClipApp.swift
@@ -1,12 +1,31 @@
 import SwiftUI
+import AppKit
 
 @main
 struct ClipApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        MenuBarExtra("Clip", systemImage: "wand.and.stars") {
+        MenuBarExtra {
             MenuBarMenuView()
+        } label: {
+            MenuBarIcon()
+        }
+    }
+}
+
+private struct MenuBarIcon: View {
+    var body: some View {
+        if let url = Bundle.main.url(forResource: "MenuBarIcon", withExtension: "svg"),
+           let nsImage = NSImage(contentsOfFile: url.path) {
+            let _ = { nsImage.isTemplate = true }()
+            Image(nsImage: nsImage)
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 18, height: 18)
+        } else {
+            Image(systemName: "wand.and.stars")
         }
     }
 }
